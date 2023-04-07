7b020a72d1cdfe2494eb9c3458981705664b0732
Vaness Krylov <i.krylov96@gmail.com>
2023-04-07 23:01:12 +0300

    edits

--- todo-app.js
+++ todo-app.js
@@ -1,21 +1,34 @@
 const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
-const row = document.querySelector('.row');
+const addButton = document.getElementById('btn');
 
-function addTask() {
-    if (inputBox.value === '') {
-        alert('You must write something!');
-    } else {
+function addTask(taskText) {
+    if (taskText) {
         let li = document.createElement('li');
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         let span = document.createElement('span');
         span.innerHTML = '\u00d7';
         li.appendChild(span);
+        inputBox.focus();
+        saveData();
+    }
+}
+
+function validateInput () {
+    if (inputBox.value.length > 3) {
+        addTask(inputBox.value);
+    } else {
+        alert('You must write valid text!');
     }
     inputBox.value = '';
-    saveData();
 }
+
+inputBox.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        validateInput();
+    }
+});
 
 listContainer.addEventListener('click', function (e) {
     if (e.target.tagName === 'LI') {
@@ -27,6 +40,7 @@ listContainer.addEventListener('click', function (e) {
     }
 }, false);
 
+addButton.addEventListener('click', validateInput);
 
 function saveData() {
     localStorage.setItem('data', listContainer.innerHTML);
